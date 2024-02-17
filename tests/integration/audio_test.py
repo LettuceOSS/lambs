0cad17cd9d17909b6bc5bc86c58e6babf33f1a7e
Thomas Margnac <t.margnac@gmail.com>
2024-02-17 20:44:56 +0100

    Audio test integration done

--- tests/integration/audio_test.py
+++ tests/integration/audio_test.py
@@ -1,5 +1,5 @@
 import requests
-import json
+from zipfile import ZipFile
 import pytest
 
 api_url = "http://localhost:80"
@@ -14,14 +14,25 @@ def test_get_audio_route():
         contents = f.readlines()
     text = " ".join(contents)
     # Posting our text
-    resp = requests.post(
+    resp_1 = requests.post(
         url=api_url + "/audio/generate",
         json={
              "text": text
         }
     )
-    assert resp.status_code == 200
-    assert ("audio_id" in resp.json()) == True
+    assert resp_1.status_code == 200
+    assert ("audio_id" in resp_1.json()) == True
+    # Getting generated file /audio/
+    resp_2 = requests.get(
+        url=api_url + "/audio/" + resp_1.json()["audio_id"]
+    )
+    file_path = resp_1.json()["audio_id"] + ".zip"
+    open(file_path, "wb").write(resp_2.content)
+    # Verifying number of files
+    with ZipFile(file_path) as archive:
+        count = len(archive.infolist())
+    assert resp_2.status_code == 200
+    assert count == 6
 
 if __name__ == "__main__":
 	pytest.main()
